c3d5a482a3ec4be9ccf3031b518311999f68073c
Adam Nielsen <malvineous@shikadi.net>
2010-01-01 19:14:28 +1000

    Added/updated CMF, DROv1 and DROv2 tests

--- test/playertest.cpp
+++ test/playertest.cpp
@@ -77,6 +77,7 @@ static const char *filelist[] = {
   "RI051.RIX",		// Softstar RIX OPL Music
   "DUNE19.ADL",		// Westwood ADL
   "DEMO4.JBM",		// JBM Adlib Music
+  "dro_v2.dro",         // DOSBox DRO v2.0
   NULL
 };
 
