1ee2825f25dc6af16ff2a5fee8d86af05b941c29
Alexander Miller <alex.miller@gmx.de>
2020-03-28 20:56:40 +0100

    Fix OOB write in CdroPlayer::load() and OOB reads in update()
    In src/dro.cpp, the code allocates a buffer with a length
    read from the .dro file, and stores the first three bytes in
    the buffer without checking. This results in an OOB write if
    the recorded length is shorter (issue #99). Fail load() early
    when such a small size is requested. (Very large sizes get
    mapped to negative values and are rejected, too.)
    
    Also, in the update() method, the code can read up to two
    bytes past the end of the same buffer. Add length checks to
    avoid that.
    
    Fixes: https://github.com/adplug/adplug/issues/99

--- src/dro.cpp
+++ src/dro.cpp
@@ -73,6 +73,10 @@ bool CdroPlayer::load(const std::string &filename, const CFileProvider &fp)
 
 	f->ignore(4);	// Length in milliseconds
 	this->iLength = f->readInt(4); // stored in file as number of bytes
+	if (this->iLength < 3) {
+		fp.close(f);
+		return false;
+	}
 
 	this->data = new uint8_t[this->iLength];
 
@@ -150,12 +154,14 @@ bool CdroPlayer::update()
 
 		// Short delay
 		if (iIndex == this->iCmdDelayS) {
+			if (this->iPos >= this->iLength) return false;
 			iValue = this->data[this->iPos++];
 			this->iDelay = iValue + 1;
 			return true;
 
 		// Long delay
 		} else if (iIndex == this->iCmdDelayL) {
+			if (this->iPos + 1 >= this->iLength) return false;
 			iValue = this->data[this->iPos] | (this->data[this->iPos + 1] << 8);
 			this->iPos += 2;
 			this->iDelay = (iValue + 1);
@@ -168,8 +174,10 @@ bool CdroPlayer::update()
 		// Normal write
 		} else {
 			if (iIndex == 0x04) {
+				if (this->iPos+1 >= this->iLength) return false;
 				iIndex = this->data[this->iPos++];
 			}
+			else if (this->iPos >= this->iLength) return false;
 			iValue = this->data[this->iPos++];
 			this->opl->write(iIndex, iValue);
 		}
