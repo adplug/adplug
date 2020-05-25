adplug/adplug

commit f20a0560bc40f95eab887a9edbbbfe79bff9cd3c

Fixed volume handling, by adding conversion table.

src/amd.cpp
@@ -39,6 +39,13 @@ bool CamdLoader::load(const std::string &filename, const CFileProvider &fp)
   int i, j, k, t, numtrax, maxi = 0;
   unsigned char buf, buf2, buf3;
   const unsigned char convfx[10] = {0,1,2,9,17,11,13,18,3,14};
+  const unsigned char convvol[64] = {
+    0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 9, 9, 0xa, 0xa, 0xb,
+    0xc, 0xc, 0xd, 0xe, 0xe, 0xf, 0x10, 0x10, 0x11, 0x12, 0x13, 0x14, 0x14,
+    0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x21,
+    0x22, 0x23, 0x25, 0x26, 0x28, 0x29, 0x2b, 0x2d, 0x2e, 0x30, 0x32, 0x35,
+    0x37, 0x3a, 0x3c, 0x3f
+  };
 
   // file validation section
   if(fp.filesize(f) < 1072) { fp.close(f); return false; }
@@ -161,6 +168,15 @@ bool CamdLoader::load(const std::string &filename, const CFileProvider &fp)
 	  tracks[i][j].command = 10;
 	  tracks[i][j].param1 = 0;
 	}
+      }
+
+      // fix volume
+      if(tracks[i][j].command == 17) {
+	int vol = convvol[tracks[i][j].param1 * 10 + tracks[i][j].param2];
+
+	if(vol > 63) vol = 63;
+	tracks[i][j].param1 = vol / 10;
+	tracks[i][j].param2 = vol % 10;
       }
     }
 

commit a41dd676a1056f7b122509e59959efe1d350dd17

amd.cpp: Fix OOB access in volume conversion
Clamp volume before (not after!) conversion to avoid accessing
the lookup table with invalid indices.

This fixes out of bounds reads for the following fuzzing samples:
  i-110_025.amd, i-110_026.amd, i-110_028.amd, i-110_083.amd,
  i-110_087.amd, i-110_088.amd, i-110_127.amd, i-110_141.amd,
  i-110_175.amd.

The following samples now load successfully, but crash during
playback instead:
  i-110_154.amd, i-110_171.amd.

Bug: https://github.com/adplug/adplug/issues/110
Fixes: f20a0560bc40 ("Fixed volume handling, by adding conversion table.")

src/amd.cpp
@@ -172,9 +172,11 @@ bool CamdLoader::load(const std::string &filename, const CFileProvider &fp)
 
       // fix volume
       if(tracks[i][j].command == 17) {
-	int vol = convvol[tracks[i][j].param1 * 10 + tracks[i][j].param2];
+	int vol = tracks[i][j].param1 * 10 + tracks[i][j].param2;
 
-	if(vol > 63) vol = 63;
+	if (vol > 63) vol = 63;
+	else vol = convvol[vol];
+
 	tracks[i][j].param1 = vol / 10;
 	tracks[i][j].param2 = vol % 10;
       }