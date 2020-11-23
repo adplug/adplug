adplug/adplug

commit 3f05bb4c7aa298e968ada8967e7f5ed521e9f165

Fix excessive memory allocations in multiple loaders (issue #100)
Several loaders use sizes read from file data unchecked,
sometimes even as signed values, or compute them in ways that
can wrap around or get negative, and then use these values
to try to allocate memory. This may fail because the maximum
allocation size is exceeded or use large amounts of memory.

* In src/cmf.cpp, the song length is computed without checking
  the validity of the music data's offset. It can become
  negative if the offset is greater than the file size.
  Add the missing check.
* In src/dro.cpp, the last commit already guarded against
  negative size values. Limit that further to not exceed the
  remaining length of the file.
* In src/dro2.cpp, check that the computation of iLength doesn't
  overflow and that the result is no more than the available
  data.
* In src/imf.cpp, also check the size computation. There are
  different cases to consider here, so it might not be
  immediately obvious: The check ensures that in both cases
  there is no wraparound (computing size or footerlen), there
  is at least one data record (as required by update()), and
  if there is a footer, the stated data size is a multiple of
  the record size and doesn't exceed the available data.
* In src/raw.cpp, a wraparound occurs if the file is shorter
  than the header size, so fail if that happens.
* Likewise in src/xad.cpp.

Fixes: https://github.com/adplug/adplug/issues/100
Reported-by: Lionel Debroux <lionel_debroux@yahoo.fr>

src/imf.cpp
@@ -89,6 +89,10 @@ bool CimfPlayer::load(const std::string &filename, const CFileProvider &fp)
   else
     fsize = f->readInt(2);
   flsize = fp.filesize(f);
+  if (mfsize + 4 > flsize || fsize >= flsize - 2 - mfsize || fsize & 3) {
+    fp.close(f);        // truncated file or bad size record
+    return false;
+  }    
   if(!fsize) {		// footerless file (raw music data)
     if(mfsize)
       f->seek(-4, binio::Add);

src/cmf.cpp
@@ -209,6 +209,10 @@ bool CcmfPlayer::load(const std::string &filename, const CFileProvider &fp)
 	// Load the MIDI data into memory
   f->seek(this->cmfHeader.iMusicOffset);
   this->iSongLen = fp.filesize(f) - this->cmfHeader.iMusicOffset;
+  if (this->iSongLen <= 0) { // invalid offset value
+    fp.close(f);
+    return false;
+  }
   this->data = new unsigned char[this->iSongLen];
   f->readString((char *)data, this->iSongLen);
 

src/dro.cpp
@@ -73,7 +73,7 @@ bool CdroPlayer::load(const std::string &filename, const CFileProvider &fp)
 
 	f->ignore(4);	// Length in milliseconds
 	this->iLength = f->readInt(4); // stored in file as number of bytes
-	if (this->iLength < 3) {
+	if (this->iLength < 3 || this->iLength > fp.filesize(f) - f->pos()) {
 		fp.close(f);
 		return false;
 	}

src/dro2.cpp
@@ -64,7 +64,13 @@ bool Cdro2Player::load(const std::string &filename, const CFileProvider &fp)
 		return false;
 	}
 
-	this->iLength = f->readInt(4) * 2; // stored in file as number of byte pairs
+	this->iLength = f->readInt(4); // should better use an unsigned type
+	if (this->iLength <= 0 || this->iLength >= 1<<30 ||
+	    this->iLength > fp.filesize(f) - f->pos()) {
+		fp.close(f);
+		return false;
+	}
+	this->iLength *= 2; // stored in file as number of byte p
 	f->ignore(4);	// Length in milliseconds
 	f->ignore(1);	/// OPL type (0 == OPL2, 1 == Dual OPL2, 2 == OPL3)
 	int iFormat = f->readInt(1);

src/raw.cpp
@@ -51,7 +51,9 @@ bool CrawPlayer::load(const std::string &filename, const CFileProvider &fp)
 
   // load section
   this->clock = f->readInt(2); // clock speed
-  this->length = (fp.filesize(f) - 10) / 2; // Wraithverge: total data-size.
+  this->length = fp.filesize(f);
+  if (this->length <= 10) { fp.close (f); return false; }
+  this->length = (this->length - 10) / 2; // Wraithverge: total data-size.
   this->data = new Tdata [this->length];
   bool tagdata = false;
   title[0] = 0;

src/xad.cpp
@@ -82,7 +82,13 @@ bool CxadPlayer::load(const std::string &filename, const CFileProvider &fp)
 	else
 	{
 		// get file size
-		tune_size = fp.filesize(f) - 80;
+		tune_size = fp.filesize(f);
+		if (tune_size <= 80)
+		{
+			fp.close(f);
+			return false;
+		}
+		tune_size -= 80;
 	}
 
 	// load()

commit efe23bac8c362b3d6791e3b55501268e3f8fda57

imf: Allow empty footer for imf files with size header
Commit 3f05bb4c7aa2 ("Fix excessive memory allocations in multiple
loaders (issue #100)") was written with the assumption that all
files with a size specifier have a footer. The check added in that
commit requires at least one byte after the song data.

This assumption is wrong, so adjust the check accordingly.

Link: https://hydrogenaud.io/index.php?topic=119501.0
Fixes: 3f05bb4c7aa2 ("Fix excessive memory allocations in multiple loaders (issue #100)")
Closes: https://github.com/adplug/adplug/pull/117
Closes: https://github.com/adplug/adplug/issues/115

src/imf.cpp
@@ -89,7 +89,7 @@ bool CimfPlayer::load(const std::string &filename, const CFileProvider &fp)
   else
     fsize = f->readInt(2);
   flsize = fp.filesize(f);
-  if (mfsize + 4 > flsize || fsize >= flsize - 2 - mfsize || fsize & 3) {
+  if (mfsize + 4 > flsize || fsize > flsize - 2 - mfsize || fsize & 3) {
     fp.close(f);        // truncated file or bad size record
     return false;
   }    