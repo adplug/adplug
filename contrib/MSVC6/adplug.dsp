adplug/adplug

commit 99bb4a14a11b06a8e7f7ed16068d3dbc046da13f

amd.h: Fix string get...() methods
Note that std::string(charptr, 0, len) is really
std::string(std::string(charptr), 0, len), so the inner
constructor is not bounded by len and can read out of bounds.
Fix that by using std::string(charptr, strnlen(charptr, len))
instead.

We might want to define a helper function for that somewhere
since converting a fixed length array to std::string is a
common problem.

Also check for a valid instrument number in getinstruments()
while at it.

src/amd.h
@@ -20,6 +20,7 @@
  */
 
 #include "protrack.h"
+#include <string.h> // for strnlen
 
 class CamdLoader: public CmodPlayer
 {
@@ -34,16 +35,16 @@ public:
 	float getrefresh();
 
 	std::string gettype()
-	{ return std::string("AMUSIC Adlib Tracker"); };
+	{ return std::string("AMUSIC Adlib Tracker"); }
 	std::string gettitle()
-	{ return std::string(songname,0,24); };
+	{ return std::string(songname, strnlen(songname, sizeof(songname))); }
 	std::string getauthor()
-	{ return std::string(author,0,24); };
+	{ return std::string(author, strnlen(author, sizeof(author))); }
 	unsigned int getinstruments()
-	{ return 26; };
+	{ return 26; }
 	std::string getinstrument(unsigned int n)
-	{ return std::string(instname[n],0,23); };
+	{ return n < getinstruments() ? std::string(instname[n], strnlen(instname[n], sizeof(instname[n]))) : std::string(); }
 
 private:
-	char songname[24],author[24],instname[26][23];
+	char songname[24], author[24], instname[26][23];
 };

commit 415b0fb995c752d31ff6adc40c66342c2daf6fd2

Allow building on platforms that don't have strnlen()
The function strnlen() isn't part of any standard. Add a header
file "strnlen.h" that prefers the system implementation of strnlen()
or strnlen_s() if available but also provides a fallback solution.

Fixes: 99bb4a14a11b ("amd.h: Fix string get...() methods")
Fixes: fa08b20047af ("Fix undefined behavior, division by zero, and OOB accesses in src/mid.cpp")
Fixes: d7f3a047e423 ("Add missing checks while loading .bmf files (CxadbmfPlayer, src/bmf.cpp)")

Makefile.am
@@ -161,6 +161,7 @@ pkginclude_HEADERS = \
 	src/vgm.h \
 	src/sop.h \
 	src/herad.h \
+	src/strnlen.h \
 	src/load_helper.h
 
 #############

contrib/MSVC6/adplug.dsp
@@ -502,6 +502,10 @@ SOURCE=.\sng.h
 # End Source File
 # Begin Source File
 
+SOURCE=.\strnlen.h
+# End Source File
+# Begin Source File
+
 SOURCE=.\u6m.h
 # End Source File
 # Begin Source File

contrib/Watcom/src.bt
@@ -19,7 +19,7 @@ include_INST = protrack.h a2m.h amd.h d00.h dfm.h hsc.h hsp.h imf.h ksm.h &
 mid.h mkj.h mtk.h rad.h raw.h s3m.h sa2.h sng.h bam.h xad.h mad.h fmc.h &
 bmf.h flash.h hybrid.h hyp.h psi.h rat.h adplug.h emuopl.h fmopl.h realopl.h &
 analopl.h diskopl.h player.h opl.h silentopl.h lds.h adtrack.h cff.h dtm.h &
-dmo.h u6m.h rol.h database.h fprovide.h players.h xsm.h dro.h
+dmo.h u6m.h rol.h database.h fprovide.h players.h xsm.h dro.h strnlen.h
 
 lib_INST = adplug.lib
 

contrib/vs2015/adplug/adplug.vcxproj
@@ -147,6 +147,7 @@
     <ClInclude Include="..\..\..\src\sa2.h" />
     <ClInclude Include="..\..\..\src\silentopl.h" />
     <ClInclude Include="..\..\..\src\sng.h" />
+    <ClInclude Include="..\..\..\src\strnlen.h" />
     <ClInclude Include="..\..\..\src\surroundopl.h" />
     <ClInclude Include="..\..\..\src\temuopl.h" />
     <ClInclude Include="..\..\..\src\u6m.h" />

src/amd.h
@@ -19,8 +19,8 @@
  * amd.h - AMD Loader by Simon Peter <dn.tlp@gmx.net>
  */
 
+#include "strnlen.h"
 #include "protrack.h"
-#include <string.h> // for strnlen
 
 class CamdLoader: public CmodPlayer
 {

src/bmf.cpp
@@ -42,7 +42,7 @@
             it can be 380 or 382.
 */
 
-#include <cstring>
+#include "strnlen.h"
 #include "bmf.h"
 #include "debug.h"
 

src/mid.cpp
@@ -75,6 +75,7 @@
  *  SD - the SCI Decoder (to get all .sci out of the Sierra files)
  */
 
+#include "strnlen.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>

src/strnlen.h
@@ -1,0 +1,60 @@
+/*
+ * AdPlug - Replayer for many OPL2/OPL3 audio file formats.
+ * Copyright (C) 1999 - 2020 Simon Peter, <dn.tlp@gmx.net>, et al.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * strnlen.h - Provide strnlen() if not available in the system library,
+ *             by Alexander Miller <alex.miller@gmx.de>.
+ */
+#ifndef ADPLUG_STRNLEN_H
+#define ADPLUG_STRNLEN_H
+
+/*
+strnlen() is a useful function, available in GNU libc and on newer
+Windows versions, but it's not part of any standard and many systems
+don't support it. The C11 standard defines strnlen_s() with the
+same functionality, but as with all bounds-checked functions, support
+is still optional.
+
+This header provides strnlen() for all systems such that the first
+available implementation from the following list is used:
+1. the system's strlen() (defined as a macro or function),
+2. the system's strlen_s() (macro or function),
+3. function template from this header.
+*/
+
+#define __STDC_WANT_LIB_EXT1__ 1
+#include <string.h>
+
+#ifndef strnlen
+#ifndef strnlen_s
+template <class Char>
+size_t strnlen_s(const Char *s, size_t size)
+{
+	size_t i = 0;
+	while (i < size && s[i]) i++;
+	return i;
+}
+#endif // strnlen_s
+
+template <class Char>
+static inline size_t strnlen(const Char *s, size_t size)
+{
+	return strnlen_s(s, size);
+}
+#endif // strnlen
+
+#endif // ADPLUG_STRNLEN_H