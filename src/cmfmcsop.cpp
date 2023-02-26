a365390c65b657e87bc57b0dad1c7d67a2fa8264
Stian Sebastian Skjelstad <stian.skjelstad@gmail.com>
2023-02-26 15:22:56 +0100

    Fix these warnings and simplify some logic by using unsigned datatypes instead of signed at some locations. (#146)
    src/cmfmcsop.cpp: In member function ‘bool CcmfmacsoperaPlayer::loadInstruments(binistream*, int)’:
    src/cmfmcsop.cpp:200:35: warning: comparison of integer expressions of different signedness: ‘int’ and ‘long unsigned int’ [-Wsign-compare]
      200 |                 for (int j = 0; j < sizeof(loadOffsets)/sizeof(loadOffsets[0]); ++j) {
          |                                 ~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    src/cmfmcsop.cpp: In member function ‘bool CcmfmacsoperaPlayer::advanceRow()’:
    src/cmfmcsop.cpp:415:80: warning: comparison of integer expressions of different signedness: ‘int’ and ‘long unsigned int’ [-Wsign-compare]
      415 |                                 if (currentOrderIndex < 0 || currentOrderIndex >= sizeof(patternOrder) / sizeof(patternOrder[0]))
          |                                                              ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    src/cmfmcsop.cpp:422:66: warning: comparison of integer expressions of different signedness: ‘int16_t’ {aka ‘short int’} and ‘std::vector<std::vector<CcmfmacsoperaPlayer::NoteEvent> >::size_type’ {aka ‘long unsigned int’} [-Wsign-compare]
      422 |                         } while (patternOrder[currentOrderIndex] >= patterns.size()); // loop to skip invalid pattern references
          |                                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~
    src/cmfmcsop.cpp:429:41: warning: comparison of integer expressions of different signedness: ‘int’ and ‘std::vector<CcmfmacsoperaPlayer::NoteEvent>::size_type’ {aka ‘long unsigned int’} [-Wsign-compare]
      429 |                 if (currentPatternIndex < p.size() && p[currentPatternIndex].row == currentRow && p[currentPatternIndex].note == 1) {
          |                     ~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~
    src/cmfmcsop.cpp: In member function ‘virtual bool CcmfmacsoperaPlayer::update()’:
    src/cmfmcsop.cpp:467:36: warning: comparison of integer expressions of different signedness: ‘int’ and ‘std::vector<CcmfmacsoperaPlayer::NoteEvent>::size_type’ {aka ‘long unsigned int’} [-Wsign-compare]
      467 |         for (; currentPatternIndex < p.size() && p[currentPatternIndex].row == currentRow; ++currentPatternIndex) {
          |                ~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~

--- src/cmfmcsop.cpp
+++ src/cmfmcsop.cpp
@@ -197,7 +197,7 @@ bool CcmfmacsoperaPlayer::loadInstruments(binistream* f, int nrOfInstruments)
 	};
 
 	for (int i = 0; i < nrOfInstruments; ++i) {
-		for (int j = 0; j < sizeof(loadOffsets)/sizeof(loadOffsets[0]); ++j) {
+		for (unsigned int j = 0; j < sizeof(loadOffsets)/sizeof(loadOffsets[0]); ++j) {
 			int v = f->readInt(2);
 			if (loadOffsets[j] >= 0 ) {
 				*(int16_t*)((char*)&instruments[i] + loadOffsets[j]) = v;
@@ -403,16 +403,16 @@ void CcmfmacsoperaPlayer::setVolume(int channelNr, int vol)
 bool CcmfmacsoperaPlayer::advanceRow()
 {
 	for (;;) {
-		if (currentRow < 0 || ++currentRow >= 64) {
+		if (++currentRow >= 64) {
 			// next pattern
 			currentRow = 0;
 			currentPatternIndex = 0;
 
 			do {
-				currentOrderIndex++;
+				currentOrderIndex++; // overflows ~0 into 0 when needed
 
 				// check bounds
-				if (currentOrderIndex < 0 || currentOrderIndex >= sizeof(patternOrder) / sizeof(patternOrder[0]))
+				if (currentOrderIndex >= (sizeof(patternOrder) / sizeof(patternOrder[0])))
 					return false;
 
 				// end of song?
@@ -421,13 +421,13 @@ bool CcmfmacsoperaPlayer::advanceRow()
 
 			} while (patternOrder[currentOrderIndex] >= patterns.size()); // loop to skip invalid pattern references
 
-			AdPlug_LogWrite("order %d, pattern %d\n", currentOrderIndex, patternOrder[currentOrderIndex]);
+			AdPlug_LogWrite("order %u, pattern %d\n", currentOrderIndex, patternOrder[currentOrderIndex]);
 		}
 
 		// check for pattern break
 		const Pattern &p = patterns[patternOrder[currentOrderIndex]];
 		if (currentPatternIndex < p.size() && p[currentPatternIndex].row == currentRow && p[currentPatternIndex].note == 1) {
-			currentRow = -1;
+			currentRow = 64;
 		}
 		else // no pattern break, done!
 			break;
@@ -459,7 +459,7 @@ void CcmfmacsoperaPlayer::processNoteEvent(const CcmfmacsoperaPlayer::NoteEvent 
 
 bool CcmfmacsoperaPlayer::update()
 {
-	AdPlug_LogWrite( "%2d: ", currentRow);
+	AdPlug_LogWrite( "%2u: ", currentRow);
 
 	const Pattern& p = patterns[patternOrder[currentOrderIndex]];
 
@@ -491,8 +491,8 @@ bool CcmfmacsoperaPlayer::update()
 
 void CcmfmacsoperaPlayer::resetPlayer()
 {
-	currentRow = -1;
-	currentOrderIndex = -1;
+	currentRow = 64;
+	currentOrderIndex = ~0;
 	advanceRow();
 }
 
