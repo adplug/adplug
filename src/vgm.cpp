a961a3ad6cf918fd5978a54d1cb5294c7d7ce3f9
Dmitry Smagin <dmitry.s.smagin@gmail.com>
2026-05-28 14:24:24 +0300

    VGM: Add support for gzipped .vgz files (#259)
    - Modified existing vgm.cpp to load the entire file into the buffer first
    - This is needed, because libbinio can't read from memory buffers, only from files (at least, without modifications)
    - Simple ungzip.c added to unpack the file if it's .vgz
    - Further on the file is treated as regular .vgm
    - Added a couple of vgz into test, all pass

--- src/vgm.cpp
+++ src/vgm.cpp
@@ -1,17 +1,17 @@
 /*
  * Adplug - Replayer for many OPL2/OPL3 audio file formats.
  * Copyright (C) 1999 - 2005 Simon Peter, <dn.tlp@gmx.net>, et al.
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -22,9 +22,6 @@
  * https://vgmrips.net/wiki/VGM_Specification
  * http://www.smspower.org/uploads/Music/vgmspec160.txt
  * http://www.smspower.org/uploads/Music/gd3spec100.txt
- *
- * TODO:
- * - Add support for *.VGZ (GZip compressed files)
  */
 
 #include <cstring>
@@ -32,6 +29,22 @@
 #include <stdlib.h>
 
 #include "vgm.h"
+#include "ungzip.h"
+
+/*** Local helpers — VGM is always little-endian ***************************/
+
+static inline uint8_t vgm_u8(const uint8_t *b, int p)
+{
+	return b[p];
+}
+
+static inline uint32_t vgm_u32(const uint8_t *b, int p)
+{
+	return (uint32_t)b[p]
+	     | ((uint32_t)b[p + 1] <<  8)
+	     | ((uint32_t)b[p + 2] << 16)
+	     | ((uint32_t)b[p + 3] << 24);
+}
 
 /*** public methods *************************************/
 
@@ -40,15 +53,25 @@ CPlayer *CvgmPlayer::factory(Copl *newopl)
 	return new CvgmPlayer(newopl);
 }
 
-void fillGD3Tag(binistream *f, wchar_t *data)
+/* Read one null-terminated UTF-16LE string from buf at position pos.
+ * Returns the new position after the terminating null character. */
+static int fillGD3Tag(const uint8_t *buf, long buflen, int pos, wchar_t *data)
 {
-	uint16_t chr, cnt = 0;
+	uint16_t chr;
+	int cnt = 0;
 	do
 	{
-		chr = f->readInt(2);
-		data[cnt < 256 ? cnt : 255] = cnt < 256 ? chr : 0;
+		if (pos + 2 > (int)buflen)
+		{
+			data[cnt < 256 ? cnt : 255] = 0;
+			break;
+		}
+		chr = (uint16_t)buf[pos] | ((uint16_t)buf[pos + 1] << 8);
+		pos += 2;
+		data[cnt < 256 ? cnt : 255] = (wchar_t)(cnt < 256 ? chr : 0);
 		cnt++;
-	} while (chr && !f->eof());
+	} while (chr);
+	return pos;
 }
 
 bool CvgmPlayer::load(const std::string &filename, const CFileProvider &fp)
@@ -63,133 +86,152 @@ bool CvgmPlayer::load(const std::string &filename, const CFileProvider &fp)
 		return false;
 	}
 
-	if (fp.filesize(f) < VGM_GZIP_MIN)
+	long filesize = (long)fp.filesize(f);
+	if (filesize < VGM_GZIP_MIN)
 	{
 		// File size is too small
 		fp.close(f);
 		return false;
 	}
-	char id[4];
-	f->readString(id, 4);
 
-	if ((uint8_t)id[0] == 0x1F &&
-		(uint8_t)id[1] == 0x8B)
+	// Load the entire file into a memory buffer
+	uint8_t *buf = new uint8_t[filesize];
+	f->readString((char *)buf, filesize);
+	fp.close(f);
+
+	// Decompress if this is a gzip stream (.vgz)
+	if (buf[0] == 0x1F && buf[1] == 0x8B)
 	{
-		// TODO: GZip decompression
-		fp.close(f);
-		return false;
+		long outsize = ungzip(buf, filesize, NULL, 0);
+		if (outsize <= 0)
+		{
+			delete[] buf;
+			return false;
+		}
+		uint8_t *decomp = new uint8_t[outsize];
+		long got = ungzip(buf, filesize, decomp, outsize);
+		delete[] buf;
+		if (got <= 0)
+		{
+			delete[] decomp;
+			return false;
+		}
+		buf      = decomp;
+		filesize = got;
 	}
-	if (fp.filesize(f) < VGM_HEADER_MIN)
+
+	// From here buf/filesize always hold plain (uncompressed) VGM data
+
+	if (filesize < VGM_HEADER_MIN)
 	{
 		// File size is too small
-		fp.close(f);
+		delete[] buf;
 		return false;
 	}
-	if (strncmp(id, VGM_HEADER_ID, 4))
+	if (memcmp(buf, VGM_HEADER_ID, 4) != 0)
 	{
 		// Header ID mismatch
-		fp.close(f);
+		delete[] buf;
 		return false;
 	}
-	uint32_t eof = f->readInt(4);
-	if (fp.filesize(f) != eof + OFFSET_EOF)
+
+	uint32_t eof_field = vgm_u32(buf, OFFSET_EOF);
+	if ((long)(eof_field + OFFSET_EOF) != filesize)
 	{
 		// EOF offset is incorrect
-		fp.close(f);
+		delete[] buf;
 		return false;
 	}
-	version = f->readInt(4);
+
+	version = (int)vgm_u32(buf, 0x08);
 	if (version < 0x151)
 	{
 		// Minimum supported VGM version is 1.51
-		fp.close(f);
+		delete[] buf;
 		return false;
 	}
-	f->seek(12, binio::Add);
-	samples = f->readInt(4);
-	loop_ofs = f->readInt(4);
-	loop_smp = f->readInt(4);
-	rate = f->readInt(4);
 
-	f->seek(12, binio::Add);
-	int data_ofs = f->readInt(4);
+	samples  = (int)vgm_u32(buf, 0x18);
+	loop_ofs = (int)vgm_u32(buf, OFFSET_LOOP);
+	loop_smp = (int)vgm_u32(buf, 0x20);
+	rate     = (int)vgm_u32(buf, 0x24);
+
+	int data_ofs = (int)vgm_u32(buf, OFFSET_DATA);
 	if (data_ofs < OFFSET_YM3812 - OFFSET_DATA + 4)
 	{
 		// VGM data overlays important header fields
-		fp.close(f);
+		delete[] buf;
 		return false;
 	}
+
 	clock = 0;
 	if (data_ofs >= OFFSET_YMF262 - OFFSET_DATA + 4)
-	{
-		f->seek(OFFSET_YMF262);
-		clock = f->readInt(4);
-	}
-	vgmOPL3 = clock != 0;
-	vgmDual = false;
+		clock = (int)vgm_u32(buf, OFFSET_YMF262);
+
+	vgmOPL3 = (clock != 0);
+	vgmDual  = false;
+
 	if (!vgmOPL3)
 	{
-		f->seek(OFFSET_YM3812);
-		clock = f->readInt(4);
+		clock   = (int)vgm_u32(buf, OFFSET_YM3812);
 		vgmDual = (clock & VGM_DUAL_BIT) > 0;
 	}
 	clock &= (VGM_DUAL_BIT - 1);
 	if (!clock)
 	{
 		// VGM clock is not set
-		fp.close(f);
+		delete[] buf;
 		return false;
 	}
+
 	loop_base = 0;
 	if (data_ofs >= OFFSET_LOOPBASE - OFFSET_DATA + 1)
-	{
-		f->seek(OFFSET_LOOPBASE);
-		loop_base = f->readInt(1);
-	}
+		loop_base = vgm_u8(buf, OFFSET_LOOPBASE);
+
 	loop_mod = 0;
 	if (data_ofs >= OFFSET_LOOPMOD - OFFSET_DATA + 1)
-	{
-		f->seek(OFFSET_LOOPMOD);
-		loop_mod = f->readInt(1);
-	}
+		loop_mod = vgm_u8(buf, OFFSET_LOOPMOD);
+
 	data_sz = 0;
-	f->seek(OFFSET_GD3);
-	int gd3_ofs = f->readInt(4);
+	int gd3_ofs = (int)vgm_u32(buf, OFFSET_GD3);
 	if (gd3_ofs)
 	{
-		// process GD3
-		f->seek(OFFSET_GD3 + gd3_ofs);
-		f->readString(id, 4);
-		if (!strncmp(id, GD3_HEADER_ID, 4))
+		// Process GD3 tag
+		int gd3_pos = OFFSET_GD3 + gd3_ofs;
+		if (gd3_pos + 12 <= filesize &&
+			memcmp(buf + gd3_pos, GD3_HEADER_ID, 4) == 0)
 		{
-			/* int gd3_ver = */ f->readInt(4);
-			/* int gd3_size = */ f->readInt(4);
-			fillGD3Tag(f, GD3.title_en);
-			fillGD3Tag(f, GD3.title_jp);
-			fillGD3Tag(f, GD3.game_en);
-			fillGD3Tag(f, GD3.game_jp);
-			fillGD3Tag(f, GD3.system_en);
-			fillGD3Tag(f, GD3.system_jp);
-			fillGD3Tag(f, GD3.author_en);
-			fillGD3Tag(f, GD3.author_jp);
-			fillGD3Tag(f, GD3.date);
-			fillGD3Tag(f, GD3.ripper);
-			fillGD3Tag(f, GD3.notes);
+			gd3_pos += 12; // skip: ID(4) + version(4) + length(4)
+			gd3_pos = fillGD3Tag(buf, filesize, gd3_pos, GD3.title_en);
+			gd3_pos = fillGD3Tag(buf, filesize, gd3_pos, GD3.title_jp);
+			gd3_pos = fillGD3Tag(buf, filesize, gd3_pos, GD3.game_en);
+			gd3_pos = fillGD3Tag(buf, filesize, gd3_pos, GD3.game_jp);
+			gd3_pos = fillGD3Tag(buf, filesize, gd3_pos, GD3.system_en);
+			gd3_pos = fillGD3Tag(buf, filesize, gd3_pos, GD3.system_jp);
+			gd3_pos = fillGD3Tag(buf, filesize, gd3_pos, GD3.author_en);
+			gd3_pos = fillGD3Tag(buf, filesize, gd3_pos, GD3.author_jp);
+			gd3_pos = fillGD3Tag(buf, filesize, gd3_pos, GD3.date);
+			gd3_pos = fillGD3Tag(buf, filesize, gd3_pos, GD3.ripper);
+			(void)    fillGD3Tag(buf, filesize, gd3_pos, GD3.notes);
 		}
 	}
 	else
 	{
-		f->seek(OFFSET_EOF);
-		gd3_ofs = f->readInt(4);
+		gd3_ofs = (int)vgm_u32(buf, OFFSET_EOF);
 	}
-	f->seek(OFFSET_DATA + data_ofs);
+
+	int data_start = OFFSET_DATA + data_ofs;
 	data_sz = gd3_ofs - data_ofs;
-	vgmData = new uint8_t[data_sz];
-	for (int i = 0; i < data_sz; i++)
+	if (data_sz <= 0 || data_start + data_sz > filesize)
 	{
-		vgmData[i] = f->readInt(1);
+		delete[] buf;
+		return false;
 	}
-	fp.close(f);
+
+	vgmData = new uint8_t[data_sz];
+	memcpy(vgmData, buf + data_start, data_sz);
+	delete[] buf;
+
 	loop_ofs -= data_ofs + (OFFSET_DATA - OFFSET_LOOP);
 	rewind(0);
 	return true;
@@ -325,7 +367,7 @@ std::string CvgmPlayer::getdesc()
 	char system[256]; system[0] = 0;
 	char date[256]; date[0] = 0;
 	char notes[256]; notes[0] = 0;
-	
+
 	if (GD3.game_en[0])
 	{
 		wcstombs(game, GD3.game_en, 256);
