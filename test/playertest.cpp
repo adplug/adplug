caec67baa574e220aa5a09f0d3daf0032fb0a1a6
Dmitry Smagin <dmitry.s.smagin@gmail.com>
2026-05-28 21:44:27 +0300

    VGM: Add support for OPL1 (YM3526) file type

--- test/playertest.cpp
+++ test/playertest.cpp
@@ -112,6 +112,7 @@ static const char *filelist[] = {
   "BeyondSN.vgm",	// Video Game Music 1.51 (OPL3)
   "Restart.vgz",	// Video Game Music gzip-compressed (OPL3)
   "Sonic.vgz",		// Video Game Music gzip-compressed (OPL3)
+  "RRThiel.vgz",	// Video Game Music gzip-compressed (OPL1)
   "GALWAY.SOP",		// Note Sequencer v1.0 by sopepos
   "ending.sop",		// Note Sequencer v2.0 by sopepos
   "MORNING.HSQ",	// HERAD SDB v1 (HSQ packed)
